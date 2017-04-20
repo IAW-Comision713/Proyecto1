7c2e3ff65348dda709d06ffcc47c72c7c7397e8a
dai <daianambtw@gmail.com>
2017-04-20 16:31:42 -0300

    carga dinamica de todas las opciones.

--- Proyecto1/public_html/js/cargarpartes.js
+++ Proyecto1/public_html/js/cargarpartes.js
@@ -1,27 +1,67 @@
 var personalizables;
 
+
 $(function() {
     var ajax = new AJAXInteraction("data/personalizables.json", function(data) {
         personalizables = data;
         cargarpartes(data);
     });
     ajax.doGet();
+    
+   $('.collapsible').collapsible(); 
 });
 
 function cargarpartes(data) {
     
     for(var parte in data) {
         
-        cargarmenu(parte);
-        //cargaropciones(data[parte]);
+        var item = $("<li></li>");
+        
+        cargarmenu(parte, item);
+        cargaropciones(parte, data[parte], item);
+        
+        $("#partes").append(item);
     }
 }
 
-function cargarmenu(parte) {
+function cargarmenu(parte, item) {
     
-     var partenueva = $("<li></li>").attr("class", "tab");
-     var link = $("<a></a>").attr("href", "#"+parte).text(parte);
-     partenueva.append(link);
-    $("#partes").append(partenueva);
+     var link = $("<div></div>").attr("class", "collapsible-header").text(parte);
+     item.append(link);
+}
+
+function cargaropciones(nombre, opciones, li) {
+    
+    var div = $("<div></div>").attr("class", "collapsible-body");
+    li.append(div);
+    
+    var lista = $("<ul></ul>");
+    
+    var index;
+    var item;
+    for (index = 0; index < opciones.length; ++index) {
+        
+        var nom = opciones[index];
+
+        var item = $("<li></li>");
+        var opcion = $("<div></div>").text(opciones[index].nombre);
+        opcion.attr("class", "collection-item");
+        
+        opcion.click(function(){
+            
+            actualizarReloj(nombre, nom);
+        });
+        
+        item.append(opcion);
+        lista.append(item);
+      
+    }
+    
+    div.append(lista);
+}
+
+function actualizarReloj(parte, elegido) {
+    
+    $("#"+parte).attr('src', "img/"+elegido.imagen);
 }
 
